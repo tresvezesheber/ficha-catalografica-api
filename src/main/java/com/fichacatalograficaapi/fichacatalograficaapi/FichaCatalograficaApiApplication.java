683354664137f4bb6456a3f6712b60e1b14e60f3
desenv2 <webmaster@universidadedevassouras.edu.br>
2020-09-28 17:07:37 -0300

    It's Alive

---
+++ src/main/java/com/fichacatalograficaapi/fichacatalograficaapi/FichaCatalograficaApiApplication.java
@@ -0,0 +1,13 @@
+package com.fichacatalograficaapi.fichacatalograficaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FichaCatalograficaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FichaCatalograficaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fichacatalograficaapi/fichacatalograficaapi/FichaCatalograficaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fichacatalograficaapi.fichacatalograficaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FichaCatalograficaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
