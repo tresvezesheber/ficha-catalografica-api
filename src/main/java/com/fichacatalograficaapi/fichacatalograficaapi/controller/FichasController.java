79e0587849d33ef5f96045b69ae522ada9a22432
desenv2 <webmaster@universidadedevassouras.edu.br>
2020-10-01 15:20:12 -0300

    FichasController completada

--- src/main/java/com/fichacatalograficaapi/fichacatalograficaapi/controller/FichasController.java
+++ src/main/java/com/fichacatalograficaapi/fichacatalograficaapi/controller/FichasController.java
@@ -1,27 +1,53 @@
 package com.fichacatalograficaapi.fichacatalograficaapi.controller;
 
 import com.fichacatalograficaapi.fichacatalograficaapi.model.Ficha;
-import com.fichacatalograficaapi.fichacatalograficaapi.repository.FichasRepository;
+import com.fichacatalograficaapi.fichacatalograficaapi.service.FichasService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fichas")
 public class FichasController {
 
     @Autowired
-    private FichasRepository fichasRepository;
+    private FichasService fichasService;
 
     @GetMapping
     public List<Ficha> listar() {
-        return fichasRepository.findAll();
+        return fichasService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+        Optional<Ficha> ficha = fichasService.buscar(id);
+        return ResponseEntity.status(HttpStatus.OK).body(ficha);
     }
 
     @PostMapping
-    public void salvar(@RequestBody Ficha ficha) {
-        fichasRepository.save(ficha);
+    public ResponseEntity<Void> salvar(@RequestBody Ficha ficha) {
+        ficha = fichasService.salvar(ficha);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(ficha.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        fichasService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> atualizar(@RequestBody Ficha ficha, @PathVariable("id") Long id) {
+        ficha.setId(id);
+        fichasService.atualizar(ficha);
+        return ResponseEntity.noContent().build();
+    }
+
 }
