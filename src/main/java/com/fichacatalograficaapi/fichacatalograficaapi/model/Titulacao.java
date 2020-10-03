d67858e7251b2121f3637782f5f7e1e0f9358141
Heber dos Santos Soares de Araujo Lima <hebireaper@gmail.com>
2020-10-03 20:14:37 -0300

    Correção biblioteca de validação

--- src/main/java/com/fichacatalograficaapi/fichacatalograficaapi/model/Titulacao.java
+++ src/main/java/com/fichacatalograficaapi/fichacatalograficaapi/model/Titulacao.java
@@ -1,11 +1,10 @@
 package com.fichacatalograficaapi.fichacatalograficaapi.model;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Titulacao {
