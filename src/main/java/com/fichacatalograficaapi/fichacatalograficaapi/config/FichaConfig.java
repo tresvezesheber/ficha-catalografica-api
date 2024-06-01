26d08f74aec283e413cb4d834111dcbe50c66661
tresvezesheber <hebireaper@gmail.com>
2024-06-01 13:39:47 -0300

    feat: add config class to Ficha domain class
    Created necessary beans to run the application.

---
+++ src/main/java/com/fichacatalograficaapi/fichacatalograficaapi/config/FichaConfig.java
@@ -0,0 +1,28 @@
+package com.fichacatalograficaapi.fichacatalograficaapi.config;
+
+import com.fichacatalograficaapi.fichacatalograficaapi.application.gateways.RepositorioDeFicha;
+import com.fichacatalograficaapi.fichacatalograficaapi.application.usecases.CriarFicha;
+import com.fichacatalograficaapi.fichacatalograficaapi.infra.gateways.FichaEntityMapper;
+import com.fichacatalograficaapi.fichacatalograficaapi.infra.gateways.RepositorioDeFichaJpa;
+import com.fichacatalograficaapi.fichacatalograficaapi.infra.persistence.FichaRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FichaConfig {
+
+    @Bean
+    CriarFicha criarFicha(RepositorioDeFicha repositorioDeFicha) {
+        return new CriarFicha(repositorioDeFicha);
+    }
+
+    @Bean
+    RepositorioDeFichaJpa repositorioDeFichaJpa(FichaRepository fichaRepository, FichaEntityMapper fichaEntityMapper) {
+        return new RepositorioDeFichaJpa(fichaRepository, fichaEntityMapper);
+    }
+
+    @Bean
+    FichaEntityMapper fichaEntityMapper() {
+        return new FichaEntityMapper();
+    }
+}
